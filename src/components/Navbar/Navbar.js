5cc68818eea66e607e61b626c9fb93697d954229
A-phenomenal1 <niteshch200053@gmail.com>
2022-06-20 15:33:26 +0530

    put change in subnavabr

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -6,40 +6,41 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./Navbar.css";
 
 export function SubNavbar() {
-  const [count, setCount] = useState(0);
-  console.log("SubNavBar Component rerender.");
-
-  useEffect(() => {
-    let interval = setInterval(() => {
-      setCount((prev) => prev + 1);
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [count]);
+  const [index, setIndex] = useState(0);
 
   const Taglines = [
     "25% off (Almost) Everything use! use code summer shoes",
     "Our Biggest sale is yet 50% off all summer shoes",
   ];
 
-  const increaseCount = () => {
-    let a = count + 1;
-    setCount(a);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % Taglines.length);
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [index]);
+
+  // const increaseCount = () => {
+  //   let a = count + 1;
+  //   setCount(a);
+  // };
 
   return (
     <div className="sn-container">
       <div className="sn-text">
-        <h1>{Taglines[0]}</h1>
+        <h1>{Taglines[index]}</h1>
       </div>
-      <h2>{count}</h2>
-      <button onClick={increaseCount}>Increase</button>
     </div>
   );
 }
 
 export function Navbar() {
+  const [searchText, setSearchText] = useState("abc");
+
+  const ref = useRef(0);
+
   return (
     <div className="nav-header">
       <div className="nav-topbar">
@@ -47,7 +48,14 @@ export function Navbar() {
           <h1>Footwear</h1>
         </div>
         <div className="nav-searchbar">
-          <input value="" placeholder="Search" />
+          <input
+            value={searchText}
+            onChange={(e) => {
+              // console.log(e.target.value);
+              setSearchText(e.target.value);
+            }}
+            placeholder="Search"
+          />
           <div className="nav-searchicon-cont">
             <SearchIcon />
           </div>
