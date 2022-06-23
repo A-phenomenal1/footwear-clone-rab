628d55339252188721c2962ac478a0bee8804a29
A-phenomenal1 <niteshch200053@gmail.com>
2022-06-23 13:02:40 +0530

    carousel update2

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -3,24 +3,57 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as CarouselItem } from "react-responsive-carousel";
 
+import "./Carousel.css";
+
 function Carousel() {
+  const carouselItems = [
+    {
+      imgSrc: "https://technext.github.io/footwear/images/img_bg_1.jpg",
+      title: "Mens",
+      subtitle1: "Shoes",
+      title2: "Collection",
+      subtitle2: "New Trending Shoes",
+      btnText: "Shop Collection",
+    },
+    {
+      imgSrc: "https://technext.github.io/footwear/images/img_bg_2.jpg",
+      title: "Huge",
+      subtitle1: "Sale",
+      title2: "50% Off",
+      subtitle2: "Big Sale sandals",
+      btnText: "Shop Collection",
+    },
+    {
+      imgSrc: "https://technext.github.io/footwear/images/img_bg_3.jpg",
+      title: "Mens",
+      subtitle1: "Shoes",
+      title2: "Collection",
+      subtitle2: "New Trending Shoes",
+      btnText: "Shop Collection",
+    },
+  ];
   return (
-    <div className="car-cont">
-      <CarouselItem>
-        <div>
-          <img src="https://technext.github.io/footwear/images/img_bg_1.jpg" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src="https://technext.github.io/footwear/images/img_bg_2.jpg" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src="https://technext.github.io/footwear/images/img_bg_3.jpg" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </CarouselItem>
-    </div>
+    <CarouselItem
+      showThumbs={false}
+      showStatus={false}
+      infiniteLoop={true}
+      // autoPlay={true}
+    >
+      {carouselItems.map((item, i) => {
+        return (
+          <div className="cr-container" key={i}>
+            <img src={item.imgSrc} />
+            <div className="cr-content">
+              <h1>{item.title}</h1>
+              <h3>{item.subtitle1}</h3>
+              <h2>{item.title2}</h2>
+              <h4>{item.subtitle2}</h4>
+              <button>{item.btnText}</button>
+            </div>
+          </div>
+        );
+      })}
+    </CarouselItem>
   );
 }
 
