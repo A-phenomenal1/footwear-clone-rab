9a2b1bdad7b962a08a19ee11bff6a5b04a46d8d1
A-phenomenal1 <niteshch200053@gmail.com>
2022-06-29 13:03:06 +0530

    footer partial done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 import "./Footer.css";
 
 function Footer() {
@@ -7,25 +12,42 @@ function Footer() {
     <div className="footer-cont">
       <div className="footer-top">
         <div className="footer-col">
-          <div className="footer-head"></div>
+          <div className="footer-head">About footwear</div>
+          <div className="footer-item-text">
+            Even the all-powerful Pointing has no control about the blind texts
+            it is an almost unorthographic life
+          </div>
+          <div className="footer-item-icons">
+            <TwitterIcon style={{ color: "rgba(0, 0, 0, 0.7)" }} />
+            <FacebookIcon style={{ color: "rgba(0, 0, 0, 0.7)" }} />
+            <LinkedInIcon style={{ color: "rgba(0, 0, 0, 0.7)" }} />
+            <GitHubIcon style={{ color: "rgba(0, 0, 0, 0.7)" }} />
+          </div>
+        </div>
+        <div className="footer-col">
+          <div className="footer-head">customer care</div>
+          <div className="footer-item">Contact</div>
+          <div className="footer-item">Returns/Exchange</div>
+          <div className="footer-item">Gift Voucher</div>
+          <div className="footer-item">Wishlist</div>
+          <div className="footer-item">Special</div>
+          <div className="footer-item">customer services</div>
+          <div className="footer-item">site maps</div>
+        </div>
+        <div className="footer-col">
+          <div className="footer-head">Information</div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
         </div>
         <div className="footer-col">
-          <div className="footer-head"></div>
+          <div className="footer-head">News</div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
         </div>
         <div className="footer-col">
-          <div className="footer-head"></div>
-          <div className="footer-item"></div>
-          <div className="footer-item"></div>
-          <div className="footer-item"></div>
-        </div>
-        <div className="footer-col">
-          <div className="footer-head"></div>
+          <div className="footer-head">Contact Information</div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
           <div className="footer-item"></div>
